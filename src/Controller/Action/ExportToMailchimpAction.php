d10aab604c688210d48337c327d99316fff9cf11
Igor Mukhin <igor.mukhin@gmail.com>
2019-04-09 15:13:37 +0300

    Fixed: Export create button (improper method was used)

--- src/Controller/Action/ExportToMailchimpAction.php
+++ src/Controller/Action/ExportToMailchimpAction.php
@@ -8,6 +8,7 @@ use Setono\SyliusMailchimpPlugin\Context\MailchimpConfigContextInterface;
 use Setono\SyliusMailchimpPlugin\Entity\MailchimpExportInterface;
 use Setono\SyliusMailchimpPlugin\Exporter\CustomerNewsletterExporterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
@@ -48,10 +49,6 @@ final class ExportToMailchimpAction
 
     public function __invoke(Request $request): Response
     {
-        if (false === $request->isXmlHttpRequest()) {
-            throw new AccessDeniedHttpException();
-        }
-
         if (false === $this->mailchimpConfigContext->isFullySetUp()) {
             $this->flashBag->add('error', $this->translator->trans('setono_sylius_mailchimp.ui.configure_first'));
 
@@ -59,25 +56,35 @@ final class ExportToMailchimpAction
                 'id' => $this->mailchimpConfigContext->getConfig()->getId(),
             ]);
 
-            return new JsonResponse(['redirect' => $url], Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['redirect' => $url], Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
+            } else {
+                return new RedirectResponse($url);
+            }
         }
 
+        // @todo Move to queue
         $export = $this->customerNewsletterExporter->exportNotExportedCustomers();
 
         if (null === $export) {
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['message' => 'Nothing to export.'], Response::HTTP_OK);
+            }
             $this->flashBag->add('info', $this->translator->trans('setono_sylius_mailchimp.ui.nothing_to_export'));
-
-            return new JsonResponse(['message' => 'Nothing to export.'], Response::HTTP_OK);
-        }
-
-        if (MailchimpExportInterface::COMPLETED_STATE === $export->getState()) {
+        } elseif (MailchimpExportInterface::COMPLETED_STATE === $export->getState()) {
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['message' => 'Export succeeded.'], Response::HTTP_OK);
+            }
             $this->flashBag->add('success', $this->translator->trans('setono_sylius_mailchimp.ui.export_succeeded'));
-
-            return new JsonResponse(['message' => 'Export succeeded.'], Response::HTTP_OK);
+        } else {
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(['message' => 'Export failed.'], Response::HTTP_EXPECTATION_FAILED);
+            }
+            $this->flashBag->add('error', $this->translator->trans('setono_sylius_mailchimp.ui.export_failed'));
         }
 
-        $this->flashBag->add('error', $this->translator->trans('setono_sylius_mailchimp.ui.export_failed'));
-
-        return new JsonResponse(['message' => 'Export failed.'], Response::HTTP_EXPECTATION_FAILED);
+        return new RedirectResponse(
+            $this->urlGenerator->generate('setono_sylius_mailchimp_admin_export_index')
+        );
     }
 }
