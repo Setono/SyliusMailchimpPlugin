49c88866cab06b807ad682fb68262acd4ce5f51d
Igor Mukhin <igor.mukhin@gmail.com>
2019-04-10 12:11:12 +0300

    Removed: AJAX export

--- src/Controller/Action/ExportToMailchimpAction.php
+++ src/Controller/Action/ExportToMailchimpAction.php
@@ -55,10 +55,6 @@ final class ExportToMailchimpAction
                 'id' => $this->mailchimpConfigContext->getConfig()->getId(),
             ]);
 
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(['redirect' => $url], Response::HTTP_NON_AUTHORITATIVE_INFORMATION);
-            }
-
             return new RedirectResponse($url);
         }
 
@@ -67,19 +63,10 @@ final class ExportToMailchimpAction
 
         if (null === $export) {
             $this->flashBag->add('info', $this->translator->trans('setono_sylius_mailchimp.ui.nothing_to_export'));
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(['message' => 'Nothing to export.'], Response::HTTP_OK);
-            }
         } elseif (MailchimpExportInterface::COMPLETED_STATE === $export->getState()) {
             $this->flashBag->add('success', $this->translator->trans('setono_sylius_mailchimp.ui.export_succeeded'));
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(['message' => 'Export succeeded.'], Response::HTTP_OK);
-            }
         } else {
             $this->flashBag->add('error', $this->translator->trans('setono_sylius_mailchimp.ui.export_failed'));
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(['message' => 'Export failed.'], Response::HTTP_EXPECTATION_FAILED);
-            }
         }
 
         return new RedirectResponse(
