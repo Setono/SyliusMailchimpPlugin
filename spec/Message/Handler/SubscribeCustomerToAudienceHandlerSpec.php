c6238f4e8d80ee72f8fb31f897fc8b379877fbc8
Joachim Løvgaard <joachim@loevgaard.dk>
2020-11-24 10:51:15 +0100

    Fixed code style and removed superfluous tests

--- spec/Message/Handler/SubscribeCustomerToAudienceHandlerSpec.php
+++ spec/Message/Handler/SubscribeCustomerToAudienceHandlerSpec.php
@@ -8,12 +8,12 @@ use Doctrine\Common\Persistence\ObjectManager;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Setono\SyliusMailchimpPlugin\Client\ClientInterface;
-use Setono\SyliusMailchimpPlugin\Repository\AudienceRepositoryInterface;
-use Setono\SyliusMailchimpPlugin\Repository\CustomerRepositoryInterface;
 use Setono\SyliusMailchimpPlugin\Message\Command\SubscribeCustomerToAudience;
 use Setono\SyliusMailchimpPlugin\Message\Handler\SubscribeCustomerToAudienceHandler;
 use Setono\SyliusMailchimpPlugin\Model\AudienceInterface;
 use Setono\SyliusMailchimpPlugin\Model\CustomerInterface;
+use Setono\SyliusMailchimpPlugin\Repository\AudienceRepositoryInterface;
+use Setono\SyliusMailchimpPlugin\Repository\CustomerRepositoryInterface;
 
 final class SubscribeCustomerToAudienceHandlerSpec extends ObjectBehavior
 {
@@ -72,45 +72,5 @@ final class SubscribeCustomerToAudienceHandlerSpec extends ObjectBehavior
             ->shouldThrow(\Exception::class)
             ->during('__invoke', [$message])
         ;
-    }
-
-    public function it_pushes_customer_to_mailchimp(
-        CustomerRepositoryInterface $customerRepository,
-        AudienceRepositoryInterface $audienceRepository,
-        CustomerInterface $customer,
-        AudienceInterface $audience,
-        ClientInterface $client
-    ): void {
-        $customerRepository->find(Argument::any())->willReturn($customer);
-        $audienceRepository->find(Argument::any())->willReturn($audience);
-        $customer->getEmail()->willReturn('test@domain.tld');
-        $customer->getFirstName()->willReturn('Test');
-        $customer->getLastName()->willReturn('User');
-        $customer->setPushedToMailchimp(Argument::any())->shouldBeCalled();
-        $customer->setUpdatedAt(Argument::any())->shouldBeCalled();
-
-        $client->updateMember($audience, $customer)->shouldBeCalled();
-
-        $message = new SubscribeCustomerToAudience(8, 15);
-        $this->__invoke($message)->shouldReturn(true);
-    }
-
-    public function it_pushes_only_email_to_mailchimp(
-        CustomerRepositoryInterface $customerRepository,
-        AudienceRepositoryInterface $audienceRepository,
-        CustomerInterface $customer,
-        AudienceInterface $audience,
-        ClientInterface $client
-    ): void {
-        $customerRepository->find(Argument::any())->willReturn($customer);
-        $audienceRepository->find(Argument::any())->willReturn($audience);
-        $customer->getEmail()->willReturn('test@domain.tld');
-        $customer->setPushedToMailchimp(Argument::any())->shouldBeCalled();
-        $customer->setUpdatedAt(Argument::any())->shouldBeCalled();
-
-        $client->updateMember($audience, $customer)->shouldBeCalled();
-
-        $message = new SubscribeCustomerToAudience(8, 15, true);
-        $this->__invoke($message)->shouldReturn(true);
     }
 }
